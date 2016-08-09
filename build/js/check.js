8e71c7f6b762f41b70203c6b22672c40f7f7b0e2
mfedorov1 <mfedorov1@gmail.com>
2016-08-09 14:57:15 +0300

    #6 Начинаем программировать
    выполнил задание #6 Начинаем программировать

---
+++ build/js/check.js
@@ -0,0 +1,34 @@
+function getMessage(a , b) 
+{
+  if (typeof(a) == "boolean")
+  {
+   if (a) 
+    {
+     return "Переданное GIF-изображение анимировано и содержит " +b +" кадров";
+    } else 
+    {
+     return "Переданное GIF-изображение не анимировано";
+    }
+  } 
+  if (typeof(a) == "number")  
+  {
+    return "Переданное SVG-изображение содержит " +a +" объектов и " +(b*4) +" атрибутов";
+  }
+  if (typeof(a) == "object" && a!==null)
+  { 
+    var amountOfRedPoints = a.reduce(function(sum, current) 
+    {
+    return sum + current;
+    }, 0);
+    result= "Количество красных точек во всех строчках изображения: " +amountOfRedPoints;
+    if (typeof(b) == "object" && b!==null)
+      { 
+      var artifactsSquare = a.reduce(function(sum, current, i) 
+        {
+        return sum + (current * b[i]);
+        }, 0);
+      result=  "Общая площадь артефактов сжатия: " +artifactsSquare +" пикселей";
+      }
+      return result;
+  }  
+}
