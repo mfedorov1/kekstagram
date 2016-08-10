e023de8e750ae9efd8e3253d9d8383e1371d1e71
mfedorov1 <mfedorov1@gmail.com>
2016-08-10 13:32:44 +0300

    результат в конце функции

--- build/js/check.js
+++ build/js/check.js
@@ -1,25 +1,25 @@
 function getMessage(a , b) {
   if (typeof(a) === "boolean") {
    if (a) {
-     return "Переданное GIF-изображение анимировано и содержит " +b +" кадров";
+     result = "Переданное GIF-изображение анимировано и содержит " +b +" кадров";
     } else {
-     return "Переданное GIF-изображение не анимировано";
+     result = "Переданное GIF-изображение не анимировано";
     }
   } 
   if (typeof a === "number") {
-    return "Переданное SVG-изображение содержит " + a + " объектов и " +(b*4) +" атрибутов";
+    result = "Переданное SVG-изображение содержит " + a + " объектов и " +(b*4) +" атрибутов";
   }
   if (typeof a === "object" &&  (a) ) { 
     var amountOfRedPoints = a.reduce(function(sum, current) {
-    return sum + current;
+      return sum + current;
     }, 0);
     result = "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
     if (typeof b === "object" && (b) { 
       var artifactsSquare = a.reduce(function(sum, current, i) {
         return sum + (current * b[i]);
-        }, 0);
+      }, 0);
       result = "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
       }
-      return result;
-  }  
+  }
+  return result;  
 }
