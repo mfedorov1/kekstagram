53b9d3d55d96f79cee08bd02d495792f3abf4e36
mfedorov1 <mfedorov1@gmail.com>
2016-08-10 13:36:36 +0300

    еще поправил

--- build/js/check.js
+++ build/js/check.js
@@ -1,7 +1,8 @@
 function getMessage(a , b) {
+  var result="";
   if (typeof(a) === "boolean") {
    if (a) {
-     result = "Переданное GIF-изображение анимировано и содержит " +b +" кадров";
+      result = "Переданное GIF-изображение анимировано и содержит " +b +" кадров";
     } else {
      result = "Переданное GIF-изображение не анимировано";
     }
@@ -9,12 +10,12 @@ function getMessage(a , b) {
   if (typeof a === "number") {
     result = "Переданное SVG-изображение содержит " + a + " объектов и " +(b*4) +" атрибутов";
   }
-  if (typeof a === "object" &&  (a) ) { 
+  if (typeof a === "object" &&  a ) { 
     var amountOfRedPoints = a.reduce(function(sum, current) {
       return sum + current;
     }, 0);
     result = "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
-    if (typeof b === "object" && (b) { 
+    if (typeof b === "object" && b ) { 
       var artifactsSquare = a.reduce(function(sum, current, i) {
         return sum + (current * b[i]);
       }, 0);
